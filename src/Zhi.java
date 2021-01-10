b7c743f7671d64a1fcc57be603b24592603e18b6
xiaohuahua <2404051686@qq.com>
2021-01-10 10:14:23 +0800

    方法参数的传递：按值传递

---
+++ src/Zhi.java
@@ -0,0 +1,17 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
+public class Zhi
+{
+    public static void main(String[] args)
+    {
+        int a=3;
+        ZhiChuan(a);
+        System.out.println(a);
+        //互不干扰
+    }
+    public static void ZhiChuan(int a)
+    {
+        a=90;
+        System.out.println(a);
+    }
+}
